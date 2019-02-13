61606a25e0ab5e8143604cb99febc5706ef49a3a
Niikou <reidneko@gmail.com>
2019-02-12 22:05:01 -0500

    edited my first created Flask app

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -17,6 +16,9 @@ app = Flask(__name__)
 def home():
     return 'My Home Page'
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
